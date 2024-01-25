18ed8df29d16d29189b6ba0194f052d9fa798f4e
Maicon Vieira <maiconrodrigo.vieira@.com>
2024-01-25 16:19:22 -0300

    Versão compativel com máquina host e guest.

--- scripts/timeform_links_scraping.py
+++ scripts/timeform_links_scraping.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.implicitly_wait(0.5)
-# ...
 timeform_link = "https://www.timeform.com/greyhound-racing/results/2024-01-15"
-# ...
+
 driver.get(timeform_link)
+driver.implicitly_wait(0.5)
+
 fullpage = driver.find_elements(By.XPATH, "//a[@class='waf-header hover-opacity']")
 qty = 0
 
